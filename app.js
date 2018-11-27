d664a1d6361ac841742d224aeed357286a48b619
danielsuidan <danielsuidan@gmail.com>
2018-11-27 15:19:46 +0100

    cloudinary

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
-
+app.use
 
 hbs.registerHelper('ifUndefined', (value, options) => {
   if (arguments.length < 2)
@@ -80,6 +80,9 @@ app.use('/', index);
 
 const authRoutes = require('./routes/auth');
 app.use('/auth', authRoutes);
+
+const profile = require('./routes/profile');
+app.use('profile',profile);
       
 
 module.exports = app;
